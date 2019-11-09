d085af5220d3ddd0241f6699e1943d196f59ea6b
Bryce Shaw <shaw.bryce@gmail.com>
2019-11-09 14:55:46 -0700

    challenge controller has a constructor

--- src/controller/ChallengeController.java
+++ src/controller/ChallengeController.java
@@ -1,5 +1,17 @@
 package controller;
 
+import model.Challenge;
+import view.View;
+
 public class ChallengeController {
+    private View theView;
+    private Challenge theModel;
+
+    public ChallengeController(View theView, Challenge theModel) {
+        this.theModel = theModel;
+        this.theView = theView;
+    }
+
+
 
 }
