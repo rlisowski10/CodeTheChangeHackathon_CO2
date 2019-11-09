c58a2f949021e0a245cfbdfb287b7bdb08f58f32
rlisowski10 <ryan.lisowski@outlook.com>
2019-11-09 14:31:03 -0700

    Created model and views

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,5 +1,12 @@
 package model;
 
-public class Person {
+import java.util.ArrayList;
 
+public class Person {
+    private ArrayList<Day> Day[];
+
+    public Person(int lengthOfChallenge) {
+        this.lengthOfChallenge = lengthOfChallenge;
+        //Day[] = new 
+    }
 }
