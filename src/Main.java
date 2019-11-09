fb713cc10d9152c6617b1f0b581131f7cef71ef7
rlisowski10 <ryan.lisowski@outlook.com>
2019-11-09 15:08:52 -0700

    Person problems

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,6 @@ import model.*;
 public class Main {
 
     public static void main(String[] args) {
-        
-        Day Day1 = new Day(1, 100, "car");
-
-        System.out.println(Day1);
 
     }
 }
