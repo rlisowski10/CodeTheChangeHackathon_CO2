b1235cfeab4fe05b27aa8d4c887c388c70df87b4
Mihai Robu <53987167+mihai-robu@users.noreply.github.com>
2019-11-09 15:33:02 -0700

    MR commit to resync

--- src/model/Challenge.java
+++ src/model/Challenge.java
@@ -1,5 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Challenge {
+
+    ArrayList<Person> participants;
+    int groupC02;
+    int lengthOfChallenge;
+
+    public Challenge(int numOfParticipants) {
+        participants = new ArrayList<>(numOfParticipants);
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
