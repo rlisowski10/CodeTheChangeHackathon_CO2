5f10acadbc2305d3b463f938e1c1f3b09a2711e8
Bryce Shaw <shaw.bryce@gmail.com>
2019-11-09 15:26:55 -0700

    config

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import controller.ChallengeController;
 import model.*;
+import view.*;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        ChallengeController theController = new ChallengeController();
         
         Day Day1 = new Day(1, 100, "car");
 
